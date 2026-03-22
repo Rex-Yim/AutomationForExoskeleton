02928cb203298ebc1759024146e70cdb8ecdf4e1
Rex Yim <rexyim1107@gmail.com>
2026-03-22 23:22:52 +0800

    Revert "Add final report DOCX from Final_Report.txt; remove LaTeX sources"
    This reverts commit af6798c2ada1d232ab10045bb609bbaaeb219e30.

---
+++ docs/latex/compile.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Build main.pdf (run from docs/latex after BasicTeX is installed)
+set -e
+export PATH="/Library/TeX/texbin:$PATH"
+cd "$(dirname "$0")"
+pdflatex -interaction=nonstopmode main.tex
+pdflatex -interaction=nonstopmode main.tex
+echo "Done: $(pwd)/main.pdf"
